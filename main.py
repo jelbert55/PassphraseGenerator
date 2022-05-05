1e7bbb3c53bd9aa8dae03cd2fe5e41c57808965b
Justin Elbert <jelbert@vt.edu>
2022-05-05 13:01:46 -0400

    Added Grab POS function, made code more efficient

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from colorama import Fore, Back
 colorama.init(autoreset=True) # reset style after every print statement
 
 # Set external files which contain wordlists
-nouns = 'nouns.txt'
-adjectives = 'adjectives.txt'
-verbs = 'verbs.txt'
-adverbs = 'adverbs.txt'
-articles = 'articles.txt'
-prepositions = 'prepositions.txt'
-propernouns = 'propernouns.txt'
-pronouns = 'pronouns.txt'
+Nou = 'nouns.txt'
+Adj = 'adjectives.txt'
+Ver = 'verbs.txt'
+Adv = 'adverbs.txt'
+Art = 'articles.txt'
+Prp = 'prepositions.txt'
+Ppn = 'propernouns.txt'
+Prn = 'pronouns.txt'
 
 # Possible sentence structures for each passphrase length. Add potential structures to this list in the form you see.
 structures_3 = ['AdjNouVer','NouVerAdv','VerNouAdj']
@@ -39,8 +39,20 @@ def roll_dice(number_of_rolls):
     
   return all_rolls
 
-# Read from files, return corresponding word
-def read_file(file_name,index):
+
+def grab_POS(part_of_speech):
+  index = roll_dice(number_of_rolls) #roll the dice to get the word index
+  print('Word index ' + str(roll_count) + ': ', end='')
+  for char in index: # display ints one at a time for coolness
+    sys.stdout.write(Fore.GREEN + Back.BLACK + char)
+    time.sleep(0.25)
+  print()
+
+  # # Read from the nouns file
+  # word,file_length = read_file(vars[part_of_speech],index)
+
+  file_name = vars[part_of_speech]
+
   with open(file_name,'r') as file:
     file_length = len(file.readlines())
     file.seek(0) #put the scanner/cursor thing back at the beginning of the file. 
@@ -53,14 +65,32 @@ def read_file(file_name,index):
         # Use the below print statement to check the index and the word from the list
         #print('TEST ' + file_name + ': ' + str(i)) # I CANNOT FIGURE OUT HOW TO ACCESS LINE 1. I = 0 PRINTS LINE 2 FOR SOME REASON. SET INDEX TO 0 TO SEE WHAT I MEAN
         word = file.readlines()[i-1].strip() + ' '
-        return word,file_length
+
+  return word
 
 
+  
 
 
-
-
+# Read from files, return corresponding word
+# def read_file(file_name,index):
+#   with open(file_name,'r') as file:
+#     file_length = len(file.readlines())
+#     file.seek(0) #put the scanner/cursor thing back at the beginning of the file. 
+#     if file_length < int(index):         
+#       index = str(int(index) % file_length) #if index is out of range mod it to put it back in range.
+#     for i, line in enumerate(file):
+#       if i == int(index):
+#         file.seek(0)
         
+#         # Use the below print statement to check the index and the word from the list
+#         #print('TEST ' + file_name + ': ' + str(i)) # I CANNOT FIGURE OUT HOW TO ACCESS LINE 1. I = 0 PRINTS LINE 2 FOR SOME REASON. SET INDEX TO 0 TO SEE WHAT I MEAN
+#         word = file.readlines()[i-1].strip() + ' '
+#         return word,file_length
+
+
+
+
 
 # Generates passphrase and entropy.
 def generate_pp(pp_length):
@@ -88,107 +118,34 @@ def generate_pp(pp_length):
   # Loop that separates sentence structure string into 3 char blocks which represent parts of speech
   sentence_parts = [temp_struct[i:i+3] for i in range(0,len(temp_struct),3)]
 
+  print(temp_struct)
+  print(sentence_parts)
+
   roll_count = 1
+  
   number_of_rolls = int()
+  while number_of_rolls not in [3,4,5,6]:
+    number_of_rolls = int(input('How many "dice" would you like to roll? (3-6) '))
 
   # For loop that takes a part of speech and finds the relevant random word
   for part_of_speech in sentence_parts:
 
-  # Will call each function needed and concat the passphrase generated
+    # Will call each function needed and concat the passphrase generated
     
-  #For sake of demo, no changes. Later, we can make this its own function and have functions that find the word we want from the given part of speech list, honestly we could probaly make that a single function that takes in a parameter called something like desiredWordType and that will tell the function what list to search under to find the word, just a thought though. ~ Sean
- 
-    
-################## Make this a function before adding other parts of speech #######
+    word = grab_POS(part_of_speech)
 
+    passphrase += word
 
-
-    # NOUNS
-    if part_of_speech == 'Nou':
-      while number_of_rolls not in [3,4,5,6]:
-        number_of_rolls = int(input('How many "dice" would you like to roll? (3-6) '))
-      index = roll_dice(number_of_rolls) #roll the dice to get the word index
-      print('Word index ' + str(roll_count) + ': ', end='')
-      for char in index: # display ints one at a time for coolness
-        sys.stdout.write(Fore.GREEN + Back.BLACK + char)
-        time.sleep(0.25)
-      print()
-
-      # Read from the nouns file
-      word,file_length = read_file(nouns,index)
-      passphrase += word
-
-      entropy = math.log(file_length)/math.log(2) #calculate the entropy for the word
-      entropy_list.append(entropy)
-      roll_count+=1
+    entropy = math.log(file_length)/math.log(2) #calculate the entropy for the word
+    entropy_list.append(entropy)
+    roll_count+=1
 
   
-    # VERBS
-    elif part_of_speech == 'Ver':
-      while number_of_rolls not in [3,4,5,6]:
-        number_of_rolls = int(input('How many \"dice" would you like to roll? (3-6)  '))
-      index = roll_dice(number_of_rolls) #roll the dice to get the word index
-      print('Word index ' + str(roll_count) + ': ', end='')
-      for char in index:
-        sys.stdout.write(Fore.GREEN + Back.BLACK + char)
-        time.sleep(0.25)
-      print()
-
-      # Read from the verbs file
-      word,file_length = read_file(verbs,index)
-      passphrase += word
-      
-      entropy = math.log(file_length)/math.log(2)
-      entropy_list.append(entropy)
-      roll_count+=1
-
-
-  
-    # ADJECTIVES
-    elif part_of_speech == 'Adj':
-      while number_of_rolls not in [3,4,5,6]:
-        number_of_rolls = int(input('How many \"dice" would you like to roll? (3-6)  '))
-      index = roll_dice(number_of_rolls) #roll the dice to get the word index
-      print('Word index ' + str(roll_count) + ': ', end='')
-      for char in index:
-        sys.stdout.write(Fore.GREEN + Back.BLACK + char)
-        time.sleep(0.25)
-      print()
-
-      # Read from the adjectives file
-      word,file_length = read_file(adjectives,index)
-      passphrase += word
-      
-      entropy = math.log(file_length)/math.log(2)
-      entropy_list.append(entropy)
-      roll_count+=1
-
-    # ADVERBS
-    elif part_of_speech == 'Adv':
-      while number_of_rolls not in [3,4,5,6]:
-        number_of_rolls = int(input('How many \"dice" would you like to roll? (3-6)  '))
-      index = roll_dice(number_of_rolls)
-      print('Word index ' + str(roll_count) + ': ', end='')
-      for char in index:
-        sys.stdout.write(Fore.GREEN + Back.BLACK + char)
-        time.sleep(0.25)
-      print()
-
-      # Read from the adverbs file
-      word,file_length = read_file(adverbs,index)
-      passphrase += word
-            
-      entropy = math.log(file_length)/math.log(2)
-      entropy_list.append(entropy)
-      roll_count+=1
-
- #sum entropies for each word in the passphrase
+  #sum entropies for each word in the passphrase
   total_entropy = sum(entropy_list)
   
   return passphrase, total_entropy
 
-
-############################################################  ######
       
 
   
