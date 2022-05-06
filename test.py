3fed05801ca664713060838bed10fe1d709ea817
Justin Elbert <jelbert@vt.edu>
2022-05-06 17:23:02 -0400

    post-meeting updates

---
+++ test.py
@@ -0,0 +1,20 @@
+# Rename file to main.py to use. Probably an easier way but whatever.
+
+# For experimentation on csv reading.
+
+import random, csv
+
+def read():
+    with open('nouns.csv','r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        index = random.randint(0,1000)
+        print('index:',index)
+
+        for i,line in enumerate(csv_reader):
+            if i == index:
+                return line[0]
+
+pp = read() + ' ' + read() + ' ' + read()
+
+print('pp:',pp)
